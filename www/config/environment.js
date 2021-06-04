78eb50f937be9901c39fea96a3104854c1037589
yuriy0803 <68668177+yuriy0803@users.noreply.github.com>
2021-06-04 18:16:26 +0200

    new

---
+++ www/config/environment.js
@@ -0,0 +1,75 @@
+/* jshint node: true */
+
+module.exports = function(environment) {
+    var ENV = {
+      modulePrefix: 'open-etc-pool',
+      environment: environment,
+      rootURL: '/',
+      locationType: 'hash',
+      EmberENV: {
+        FEATURES: {
+          // Here you can enable experimental features on an ember canary build
+          // e.g. 'with-controller': true
+        }
+      },
+  
+      APP: {
+        // API host and port
+        ApiUrl: '//eth.pool-node.com//',
+  
+        // HTTP mining endpoint
+        HttpHost: 'http://eth.pool-node.com',
+        HttpPort: 8888,
+  
+        // Stratum mining endpoint
+        StratumHost: 'eth.pool-node.com',
+        StratumPort: 5555,
+        StratumHost1: 'eth.pool-node.com',
+        StratumPort1: 9000,
+  
+        // The ETC network used (classic, mordor)
+        Network: 'classic',
+
+        // Fee and payout details
+        PoolFee: '0.1%',
+        PayoutThreshold: '0.05 ETH',
+  
+        // For network hashrate (change for your favourite fork)
+        BlockTime: 14,
+        BlockReward: 2.0,
+        Blockpreis: 2000000000,
+        Unit: 'ETH',
+        Currency: 'USD'
+      }
+    };
+  
+    if (environment === 'development') {
+      /* Override ApiUrl just for development, while you are customizing
+        frontend markup and css theme on your workstation.
+      */
+      ENV.APP.ApiUrl = 'http://localhost:8081/'
+      // ENV.APP.LOG_RESOLVER = true;
+      // ENV.APP.LOG_ACTIVE_GENERATION = true;
+      // ENV.APP.LOG_TRANSITIONS = true;
+      // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+      // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    }
+  
+    if (environment === 'test') {
+      // Testem prefers this...
+      ENV.locationType = 'none';
+  
+      // keep test console output quieter
+      ENV.APP.LOG_ACTIVE_GENERATION = false;
+      ENV.APP.LOG_VIEW_LOOKUPS = false;
+  
+      ENV.APP.rootElement = '#ember-testing';
+    }
+  
+    if (environment === 'production') {
+  
+    }
+  
+    return ENV;
+  };
+  
