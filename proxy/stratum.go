21d92cd682112d982b7054297621171af30a59ef
yuriy0803 <68668177+yuriy0803@users.noreply.github.com>
2021-07-26 18:28:54 +0200

    block zeit

--- proxy/stratum.go
+++ proxy/stratum.go
@@ -115,18 +115,32 @@ func (s *ProxyServer) handleTCPClient(cs *Session) error {
 func (cs *Session) handleTCPMessage(s *ProxyServer, req *StratumReq) error {
 	// Handle RPC methods
 	switch req.Method {
-	case "eth_submitLogin":
-		var params []string
-		err := json.Unmarshal(req.Params, &params)
-		if err != nil {
-			log.Println("Malformed stratum request params from", cs.ip)
-			return err
-		}
-		reply, errReply := s.handleLoginRPC(cs, params, req.Worker)
-		if errReply != nil {
-			return cs.sendTCPError(req.Id, errReply)
-		}
-		return cs.sendTCPResult(req.Id, reply)
+        // claymore -esm 1
+        case "eth_login":
+                var params []string
+                err := json.Unmarshal(req.Params, &params)
+                if err != nil {
+                        log.Println("Malformed stratum request params from", cs.ip)
+                        return err
+                }
+                reply, errReply := s.handleLoginRPC(cs, params, req.Worker)
+                if errReply != nil {
+                        return cs.sendTCPError(req.Id, errReply)
+                }
+                return cs.sendTCPResult(req.Id, reply)
+        // claymore -esm 0
+        case "eth_submitLogin":
+                var params []string
+                err := json.Unmarshal(req.Params, &params)
+                if err != nil {
+                        log.Println("Malformed stratum request params from", cs.ip)
+                        return err
+                }
+                reply, errReply := s.handleLoginRPC(cs, params, req.Worker)
+                if errReply != nil {
+                        return cs.sendTCPError(req.Id, errReply)
+                }
+                return cs.sendTCPResult(req.Id, reply)
 	case "eth_getWork":
 		reply, errReply := s.handleGetWorkRPC(cs)
 		if errReply != nil {
